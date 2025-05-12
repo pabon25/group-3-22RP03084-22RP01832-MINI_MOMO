a8424b27bf5806d7e479991df4580143a2ae2925
MASENGESHO Pacifique <139360532+pabon25@users.noreply.github.com>
2025-05-12 14:56:43 +0200

    Unchangeable Values

--- util.php
+++ util.php
@@ -15,7 +15,7 @@ class Util {
     // Africa's Talking SMS settings
     const AT_USERNAME = "sandbox";
     const AT_API_KEY = "atsk_0777ca10bd8047303f61e49baa170df0eb7c259f0f32e11ec9b4bf5edc46a5f34a9c4af8";
-    const SMS_SENDER = "MMoney Limited";
+    const SMS_SENDER = "XYZ MOMO Ltd";
 
     private $pdo;
 
